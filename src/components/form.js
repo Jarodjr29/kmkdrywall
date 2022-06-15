081816782d680d3d76bac475459ea532c7d5a9f2
Jarodjr29 <47609939+Jarodjr29@users.noreply.github.com>
2022-06-15 15:57:33 -0400

    Added A Form Component
    Added a basic form component for the Quotes page.

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,63 @@
 import React, { useState } from "react";
 
-const Form () => {
-    
+const Form = () => {
+    const [input, setInput] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        sector: '',
+        type: '',
+        prints: ''
+    })
+
+    const inputHandler = (e) =>{
+        setInput( {[e.target.name]: e.target.value})
+    }
+
+    const submit = () =>{
+        alert(input.sector)
+    }
+
+    return (
+        <div className="quoteForm">
+            <input 
+            type="text"
+            name="name"
+            onChange={inputHandler}
+            placeholder="Name"
+            value={input.name}/>
+            <br/>
+            <input 
+            type="text"
+            name="email"
+            onChange={inputHandler}
+            placeholder="Email"
+            value={input.email}/>
+            <br/>
+            <input 
+            type="text"
+            name="phone"
+            onChange={inputHandler}
+            placeholder="Phone"
+            value={input.phone}/>
+            <br/>
+            <input 
+            type="text"
+            name="sector"
+            onChange={inputHandler}
+            placeholder="Residential or Commercial?"
+            value={input.sector}/>
+            <br/>
+            <input 
+            type="text"
+            name="prints"
+            onChange={inputHandler}
+            placeholder="Prints"
+            value={input.prints}/>
+            <br/>
+            <button onClick={submit}>Submit Now</button>
+        </div>
+    )
 }
+
+export default Form
--- src/components/quote.js
+++ src/components/quote.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Form from "./form";
 
 class Quote extends Component{
     render(){
         return (
             <div className="quote">
-                <p>Quote</p>
+                <h1>Request A Quote!</h1>
+                <Form/>
             </div>
         )
     }
